e7762ade1a51b1c86785e02888405eb5e4a546bd
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-02-22 00:18:10 +0000

    divs added to section

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ const liEl = document.createElement('li'); //created li elements using createEle
 console.log(ulEl)
 ulEl.appendChild(liEl); //added li elemene to ul element using append child method
 
-const mainEl = document.querySelector('main');
-const divEl =  document.createElement('div');
-mainEl.appendChild(divEl)
+// const mainEl = document.querySelector('main');
+// console.log(mainEl)
+// const divEl =  document.createElement('div');
+// mainEl.appendChild(divEl)
+
+
+const mainEl = document.querySelector('main') 
+console.log(mainEl)
+const fragment = document.createDocumentFragment();
+Array.from('mainEl').forEach(function(divs) {                 //Mind blown! multiple divs added using createDocumentFrangment method
+    let divEl = document.createElement('div');
+    divEl.setAttribute('class','container');
+    fragment.appendChild(divEl)
+});
+
+mainEl.appendChild(fragment)
+
+
+const sectionEl = document.querySelector('section') 
+console.log(sectionEl)
+const frag = document.createDocumentFragment();
+Array.from('section').forEach(function(divs) {                 //Mind blown! multiple divs added using createDocumentFrangment method
+    let divEl = document.createElement('div');
+    divEl.setAttribute('class','container');
+    fragment.appendChild(divEl)
+});
+
+sectionEl.appendChild(fragment)
