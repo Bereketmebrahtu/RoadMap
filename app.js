ab287174b6006c8555245e6f33480f37d1e85983
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-02-22 19:27:03 +0000

    added new divs to class markup

--- app.js
+++ app.js
@@ -21,13 +21,18 @@ Array.from('mainEl').forEach(function(divs) {                 //Mind blown! mult
 mainEl.appendChild(fragment)
 
 
-const sectionEl = document.querySelector('section') 
-console.log(sectionEl)
+const markupEl = document.querySelector('.markup') 
+console.log(markupEl)
 const frag = document.createDocumentFragment();
-Array.from('section').forEach(function(divs) {                 //Mind blown! multiple divs added using createDocumentFrangment method
+Array.from('markup').forEach(function(divs) {                 //Mind blown! multiple divs added using createDocumentFrangment method
     let divEl = document.createElement('div');
     divEl.setAttribute('class','container');
     fragment.appendChild(divEl)
 });
 
-sectionEl.appendChild(fragment)
+markupEl.appendChild(fragment)
+
+const styleEl = document.querySelector('.style');
+const divEl = document.createElement('div');
+divEl.setAttribute('class', 'style-div');
+styleEl.appendChild(divEl);
