8d1c7429bd4346318f43fbf51e9168856e499c1c
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-02-24 01:06:42 +0000

    added textContent to ul element

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ulEl = document.querySelector('ul');  // grabed our ul elemnet using querySelector
 const liEl = document.createElement('li'); //created li elements using createElement
 console.log(ulEl)
+ulEl.textContent = "list items"
 ulEl.appendChild(liEl); //added li elemene to ul element using append child method
 
 // const mainEl = document.querySelector('main');
